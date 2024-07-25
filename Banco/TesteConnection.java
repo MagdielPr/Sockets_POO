7d0a1f15a6356e7286c1987a54683e7d9acb6e5e
MagdielPr <152613195+MagdielPr@users.noreply.github.com>
2024-07-24 22:07:48 -0300

    Add files via upload

---
+++ Banco/TesteConnection.java
@@ -0,0 +1,20 @@
+package Banco;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class TesteConnection {
+//CLASSE CRIADA SEGUINDO VIDEO, USAR PARA TESTAR CONEXÃO, FUNCIONA SEM PRECISAR ATIVAR O XAMP '-'
+	public static void main(String[] args) {
+		Connection con = null;
+		try {
+			con = (Connection)DriverManager.getConnection("jdbc:mysql://localhost:3306/sqlconnection","root","1234");
+			if(con!= null) {
+				System.out.println("Conectado guri!");
+			}
+		}catch (Exception e) {
+			System.out.println("Erro");
+		}
+	}
+
+}
---
+++ module-info.java
@@ -0,0 +1,10 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module Sockets {
+	requires java.sql;
+	requires com.google.gson;
+}
